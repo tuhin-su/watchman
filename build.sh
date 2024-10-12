18b989d7a9d3a861bba3cb32e3d079710bd42be5
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-12 23:57:12 +0530

    add service

--- build.sh
+++ build.sh
@@ -3,4 +3,11 @@ python3 -m venv .venv
 source .venv/bin/active
 pip install -r requrement.text
 
-pyinstaller --onefile --icon=path/to/your/icon.ico --add-data "modules:modules" main.py
+pyinstaller --onefile --icon=icon/icon.ico --add-data "modules:modules" main.py
+
+sudo mkdir /opt/watchman
+sudo mv -v dist/main /opt/watchman/
+sudo chmod +x /opt/watchman/main
+sudo cp -p watchman.service /etc/systemd/system/watchman.service
+sudo systemctl daemon-reload
+sudo systemctl enable watchman.service --now
