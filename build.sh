2f65e18bafd6cb69582fb64fd9744d84cfb8d9e8
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-13 00:06:25 +0530

    fix build scripy

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-sudo apt install pyhton3 python3-pip python3-venv
+sudo apt update -y
+sudo apt install pyhton3 python3-pip python3-venv -y
 python3 -m venv .venv
 source .venv/bin/active
 pip install -r requrement.text
