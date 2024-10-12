ecdb6d2e58eddbfe8f30dc9ba43e56f7aa85c856
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-13 00:44:07 +0530

    Uploade logger

--- modules/logger.py
+++ modules/logger.py
@@ -1,4 +1,14 @@
 import os
 
-def log(text:str):
-    os.system(f'echo "{text}" >> /var/logs/watchman/$(date +%Y-%m-%d)_watchman.log')
+def log(text: str):
+    # Define the directory path
+    log_dir = '/var/logs/watchman/'
+    # Check if the directory exists, if not, create it
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+    
+    # Define the log file path with the current date
+    log_file = os.path.join(log_dir, f'$(date +%Y-%m-%d)_watchman.log')
+    
+    # Write the log entry
+    os.system(f'echo "{text}" >> {log_file}')
